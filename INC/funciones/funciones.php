42e38836f47d02a74834159e2b587a2926d51251
Luis Zunino <zuninoluis01@gmail.com>
2020-11-07 19:34:58 -0300

    funciones.php
    se agrega una funcion que hace que segun la pagina que este se tome la palabra que identifica el archivo de esa pagina y a lo que las clases tienen su pripio nombre lo agrega en el header

--- INC/funciones/funciones.php
+++ INC/funciones/funciones.php
@@ -2,6 +2,7 @@
     //vamos a obtener el archivo en el al cual estamos llamando esta funcion
     //server lo que va a hacer es aceder a los archivos en los que esta opedado
     $archivo = basename($_SERVER["PHP_SELF"]);
-    echo $archivo;
+    //str_replace remplaza una parte de un str con otra
+    $pagina = str_replace(".php", "", $archivo);
+    return $pagina;
 }
-obtenerPaginaActual();
