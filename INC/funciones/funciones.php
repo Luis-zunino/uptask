097c610e38bb717c9202f776b38287bce15b4f77
Luis Zunino <zuninoluis01@gmail.com>
2020-11-08 13:59:42 -0300

    Update funciones.php
    se cambia el tipo de comentado

--- INC/funciones/funciones.php
+++ INC/funciones/funciones.php
@@ -1,8 +1,8 @@
 <?php function obtenerPaginaActual(){
-    //vamos a obtener el archivo en el al cual estamos llamando esta funcion
-    //server lo que va a hacer es aceder a los archivos en los que esta opedado
+    /*vamos a obtener el archivo en el al cual estamos llamando esta funcion
+    server lo que va a hacer es aceder a los archivos en los que esta opedado*/
     $archivo = basename($_SERVER["PHP_SELF"]);
-    //str_replace remplaza una parte de un str con otra
+    /*str_replace remplaza una parte de un str con otra*/
     $pagina = str_replace(".php", "", $archivo);
     return $pagina;
 }
