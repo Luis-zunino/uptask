b7b8d1c2e18d7a730ade503b95f5ec3ba73620d3
Luis Zunino <zuninoluis01@gmail.com>
2020-11-08 13:02:52 -0300

    Update footer.php
    se crea el footer

---
+++ INC/templates/footer.php
@@ -0,0 +1,11 @@
+<script src="js/sweetalert2.all.min.js"></script>
+<?php 
+//tambien tenemos que cargar el fomrulario
+//a lo que la funcion obtenerPaginaActual retorna un valor vamos a asignarlo a $actual
+$actual = obtenerPaginaActual();
+
+?>
+
+
+</body>
+</html>
