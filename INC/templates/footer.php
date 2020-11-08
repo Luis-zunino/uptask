bd0e1f6211c740d2a2a18f6e7fabbb321872c3e9
Luis Zunino <zuninoluis01@gmail.com>
2020-11-08 13:59:27 -0300

    Update footer.php
    se arreglo los espacios y ademas se cambio las comillas externas ( ' ' ) en la linea donde se llama al formulario.js

--- INC/templates/footer.php
+++ INC/templates/footer.php
@@ -1,11 +1,14 @@
 <script src="js/sweetalert2.all.min.js"></script>
-<?php 
-//tambien tenemos que cargar el fomrulario
-//a lo que la funcion obtenerPaginaActual retorna un valor vamos a asignarlo a $actual
+<?php
+/*tambien tenemos que cargar el fomrulario
+a lo que la funcion obtenerPaginaActual retorna un valor vamos a asignarlo a $actual*/
 $actual = obtenerPaginaActual();
-
+if ($actual === "crear-cuenta" || $actual === "login") {
+    echo '<script src="js/formulario.js"></script>';
+}
 ?>
 
 
 </body>
+
 </html>
