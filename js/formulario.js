dc43433263e5370787a9cf88f3c7c6c0d2681cc7
Luis Zunino <zuninoluis01@gmail.com>
2020-11-09 19:45:08 -0300

    Update formulario.js
    se soluciona errores de que no incluida a sweetalert

--- js/formulario.js
+++ js/formulario.js
@@ -22,11 +22,6 @@ function validarRegistro(e) {
         })
     } else {
         //ambos datos son correctos
-        Swal.fire({
-            icon: 'success',
-            title: 'Correcto',
-            text: 'Ingreso correcto!'
-        })
         //AJAX, VAMOS A CONSULTAR LA BASE DE DATOS, MANDAR A LLAMAR CON AJAX CON FORMDATA
         var datos = new FormData(); // nos va a permitir estructurar nuestro llamado a ajax darle una llave y un valor y enviar esos datos con ajax
         datos.append("usuario", usuario);
@@ -37,13 +32,32 @@ function validarRegistro(e) {
         //abrir la coneccion
         xhr.open("POST", "inc/modelos/modelo-admin.php", true);
         //retorno de datos
-        xhr.onload = function(){
-            if(this.status === 200){
-                console.log(JSON.parse(xhr.responseText));
+        xhr.onload = function () {
+            if (this.status === 200) {
+                var respuesta = JSON.parse(xhr.responseText);
+                //si la repsuetsa es correcta
+                if (respuesta.respuesta === "correcto") {
+                    //si es un nuevo usuario
+                    if (respuesta.tipo === "crear") {
+                        Swal.fire({
+                            title: "Usuario Creado",
+                            text: "El usuario se creó correctamente",
+                            icon: "success"
+                        });
+                    }
+                } else {
+                    //hubo un error
+                    Swal.fire({
+                        title: "Error",
+                        text: "Hubo un error",
+                        icon: "error"
+                    })
+                }
             }
-        }
+
+        } /**AJAX nos va a permitir leer los datos que se encuentren en el archivo javascript lo que es el usuario y el password, etc y lo va a comunicar con los archivos de php */
+
         //enviar la peticion
         xhr.send(datos);
-    } /**AJAX nos va a permitir leer los datos que se encuentren en el archivo javascript lo que es el usuario y el password, etc y lo va a comunicar con los archivos de php */
-
+    }
 }
