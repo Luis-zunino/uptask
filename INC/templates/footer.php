2c7d278aac69c28b9a7e64b3b5e9917a7b509afd
Luis Zunino <zuninoluis01@gmail.com>
2020-11-09 13:16:04 -0300

    Update footer.php
    se modifica la forma de acceder al seewtalert

--- INC/templates/footer.php
+++ INC/templates/footer.php
@@ -1,4 +1,4 @@
-<script src="js/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <?php
 /*tambien tenemos que cargar el fomrulario
 a lo que la funcion obtenerPaginaActual retorna un valor vamos a asignarlo a $actual*/
