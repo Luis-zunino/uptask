527bf45f6e8086e4ad25f312eb44bf43dd9f1ef4
Luis Zunino <zuninoluis01@gmail.com>
2020-11-10 16:23:54 -0300

    Update scripts.js
    se crea el input para poder crear un nuevo proyecto, ademas se agrega la opcion que al momento de dar enter se cree y luego se borre ese input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 eventListeners();
 //lista de proyecto
 var listaProyectos = document.querySelector("ul#proyectos");
-function eventListeners(){
+
+function eventListeners() {
     //boton para crear proyecto
     document.querySelector(".crear-proyecto a").addEventListener("click", nuevoProyecto);
 }
 
-function nuevoProyecto(e){
+function nuevoProyecto(e) {
     e.preventDefault();
     console.log("Presionaste en nuevo proyecto");
     //crea un <input> para el nombre del nuevo proyecto
     var nuevoProyecto = document.createElement("li");
     nuevoProyecto.innerHTML = '<input type="text" id="nuevo-proyecto">';
     listaProyectos.appendChild(nuevoProyecto);
+    //seleccionar el id con el nuevoProyecto
+    var inputNuevoProyecto = document.querySelector("#nuevo-proyecto");
+    //al presionar enter crea el proyecto
+    inputNuevoProyecto.addEventListener("keypress", function (e) {
+        var tecla = e.which || e.keyCode;
+        if (tecla === 13) {
+            guardarProyectoDB(inputNuevoProyecto.value);
+            //quiero que el imput desaparezca al momento de crear un nuevo proyecto
+            listaProyectos.removeChild(nuevoProyecto);
+        }
+    });
+}
+
+function guardarProyectoDB(nombreProyecto) {
+    console.log(nombreProyecto);
 }
